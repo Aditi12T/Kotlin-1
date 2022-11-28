46513d53d69a8f6ea0f073238789edcc4fadfb14
Aditi Tiwari <69574689+Aditi12T@users.noreply.github.com>
2022-11-28 21:40:13 +0530

    Add files via upload

---
+++ productDBtask/src/main/kotlin/com/axis/app/DbConnection.kt
@@ -0,0 +1,11 @@
+package com.axis.app
+
+import java.sql.Connection
+    import java.sql.DriverManager
+    class DbConnection {
+        fun connect(): Connection {
+            val dburl = "jdbc:mysql://localhost:3306/productdb"
+            val connection = DriverManager.getConnection(dburl, "root", "Kotlin@db31")
+            return connection
+        }
+    }
